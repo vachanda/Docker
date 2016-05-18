304dbcd0961b2e74d82c6f985a8cea099af9fd8c
Vachan DA <vachan.da@urbanladder.com>
2016-05-18 12:33:40 +0530

    Add docker file for Mongo Replica Set configuration.

---
+++ mongodb_replica_set/startUp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -x
+MONGO_LOG="/var/log/mongodb/mongod.log"
+MONGO="/usr/bin/mongo"
+MONGOD="/usr/bin/mongod"
+$MONGOD --fork --replSet $REPL_NAME --logpath $MONGO_LOG
+
+checkSlaveStatus(){
+SLAVE=$1
+$MONGO --host $SLAVE --eval db
+while [ "$?" -ne 0 ]
+do
+echo "Waiting for slave to come up..."
+sleep 15
+$MONGO --host $SLAVE --eval db
+done
+}
+
+if [ "$ROLE" == "master" ]
+then
+$MONGO --eval "rs.initiate({_id: \"${REPL_NAME}\", members:[{\"_id\":1, \"host\":\"${MASTER}:${PORT}\"}]})"
+checkSlaveStatus $SLAVE1
+$MONGO --eval "rs.add(\"${SLAVE1}:${PORT}\")"
+checkSlaveStatus $SLAVE2
+$MONGO --eval "rs.add(\"${SLAVE2}:${PORT}\")"
+fi
+
+tail -f /dev/null
