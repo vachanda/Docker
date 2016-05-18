6774f45a6ce57382b55d6168f7b629c0b6fbec53
Vachan DA <vachan.da@urbanladder.com>
2016-05-18 12:44:24 +0530

    Add slave ok to enable reads on Secondaries.

--- mongodb_replica_set/startUp.sh
+++ mongodb_replica_set/startUp.sh
@@ -4,15 +4,16 @@ MONGO_LOG="/var/log/mongodb/mongod.log"
 MONGO="/usr/bin/mongo"
 MONGOD="/usr/bin/mongod"
 $MONGOD --fork --replSet $REPL_NAME --logpath $MONGO_LOG
+$MONGO --eval "rs.slaveOk()"
 
 checkSlaveStatus(){
 SLAVE=$1
-$MONGO --host $SLAVE --eval db
+$MONGO --host $SLAVE:$PORT --eval db
 while [ "$?" -ne 0 ]
 do
 echo "Waiting for slave to come up..."
 sleep 15
-$MONGO --host $SLAVE --eval db
+$MONGO --host $SLAVE:$PORT --eval db
 done
 }
 
